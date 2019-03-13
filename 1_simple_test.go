788083e0bb8419d8e4a14024bf58748399b2b73f
Kirill Shvakov <shvakov@gmail.com>
2019-03-13 09:19:14 +0200

    new test

--- 1_simple_test.go
+++ 1_simple_test.go
@@ -1,8 +1,6 @@
 package tests
 
 import (
-	"bytes"
-	"encoding/csv"
 	"testing"
 
 	"github.com/ClickHouse-Ninja/ok"
@@ -26,37 +24,11 @@ func TestSelect1(t *testing.T) {
 	}
 }
 
-func TestArgMax(t *testing.T) {
-	const ddl = `
-	CREATE DATABASE test;
-	CREATE TABLE    test.arg_max(
-		user_id Int8
-		, value Int8
-	) Engine Memory
-	`
-	var (
-		ok = ok.Connect(t, "tcp://127.0.0.1:9000?debug=0")
-	)
-	if ok.DatabaseExists("test") {
-		t.Fatal("database 'test' is already exists")
-	}
-	defer ok.Clear()
-	if err := ok.Exec(ddl); assert.NoError(t, err) {
-		var (
-			buf bytes.Buffer
-			csv = csv.NewWriter(&buf)
-		)
-		csv.WriteAll([][]string{
-			[]string{"24", "25"},
-			[]string{"42", "52"},
-		})
-		csv.Flush()
-
-		if ok.CopyFromCSVReader(&buf, "INSERT INTO test.arg_max VALUES") {
-			var value int
-			if err := ok.DB().QueryRow("SELECT argMax(user_id, value) FROM test.arg_max").Scan(&value); assert.NoError(t, err) {
-				assert.Equal(t, 42, value)
-			}
+func TestDatabaseExists(t *testing.T) {
+	ok := ok.Connect(t, "tcp://127.0.0.1:9000?debug=0")
+	if assert.True(t, ok.DatabaseExists("system")) {
+		for _, table := range []string{"clusters", "databases", "functions", "parts", "tables"} {
+			assert.True(t, ok.TableExists("system", table))
 		}
 	}
 }
---
+++ 2_arg_min_max_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/csv"
+	"testing"
+
+	"github.com/ClickHouse-Ninja/ok"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArgMinMax(t *testing.T) {
+	const ddl = `
+	CREATE DATABASE test;
+	CREATE TABLE    test.arg_max(
+		user_id Int8
+		, value Int8
+	) Engine Memory
+	`
+	var (
+		ok = ok.Connect(t, "tcp://127.0.0.1:9000?debug=0")
+	)
+	if ok.DatabaseExists("test") {
+		t.Fatal("database 'test' is already exists")
+	}
+	defer ok.Clear()
+	if err := ok.Exec(ddl); assert.NoError(t, err) {
+		var (
+			buf bytes.Buffer
+			csv = csv.NewWriter(&buf)
+		)
+		csv.WriteAll([][]string{
+			[]string{"24", "25"},
+			[]string{"42", "52"},
+		})
+		csv.Flush()
+		if ok.CopyFromCSVReader(&buf, "INSERT INTO test.arg_max VALUES") {
+			var value int
+			if err := ok.DB().QueryRow("SELECT argMax(user_id, value) FROM test.arg_max").Scan(&value); assert.NoError(t, err) {
+				assert.Equal(t, 42, value)
+			}
+			if err := ok.DB().QueryRow("SELECT argMin(user_id, value) FROM test.arg_max").Scan(&value); assert.NoError(t, err) {
+				assert.Equal(t, 24, value)
+			}
+		}
+	}
+}
